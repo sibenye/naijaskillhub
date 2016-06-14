2d8f18657b1336a384c4c8030dade4ad91a38fe7
sibenye <sibenye@gmail.com>
2016-06-14 07:51:04 -0400

    issue 15
    added api doc to the login action

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -16,6 +16,34 @@ class Authentication extends NSH_Controller {
 		$this->load->model('Authentication_model');
 	}
 	
+	/**
+	 * @api {post} /authentication/login Authenticate User
+	 * @apiName LoginUser
+	 * @apiGroup Authentication
+	 *
+	 * @apiParam {String} username
+	 * @apiParam {String} password
+	 *
+	 * @apiSuccessExample Success-Response:
+	 * HTTP/1.1 200 OK
+	 * {
+	 *	"status" : 0,
+	 *	"message" : "success",
+	 *	"response" : {"authKey" : "...8yIepkSfHkFFSZiLujlpQL..."}
+	 * }
+	 *
+	 * @apiErrorExample Validation Error:
+	 * HTTP/1.1 400 Bad Request
+	 * {
+	 * 	"status" : 109,
+	 * 	"message" : "Invalid username or password",
+	 * 	"errorDetail" : ""
+	 * }
+	 *
+	 * @apiError 110 Validation Error
+	 * @apiError 109 Invalid username or password
+	 *
+	 */
 	function login_post()
 	{
 		try {
